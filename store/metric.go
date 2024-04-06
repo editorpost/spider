d746b66676e14ed89d1de50a95dea88887189998
estatie <estatie@proton.me>
2024-04-07 03:43:35 +0700

    Create mongo based telemetry with memory buffer flushing every minute.

---
+++ store/metric.go
@@ -0,0 +1,158 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/editorpost/donq/mongodb"
+	"log/slog"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	TelemetryDatabase   = "spider"
+	TelemetryCollection = "telemetry"
+)
+
+type MetricRow struct {
+	Job       string
+	URL       string
+	EventType string
+	ErrorInfo *string
+	Timestamp time.Time
+}
+
+type MetricStore struct {
+	collection  *mongo.Collection
+	accumulator map[string]MetricRow
+	mu          sync.Mutex
+	flushTicker *time.Ticker
+	closeChan   chan struct{}
+}
+
+// NewMetricStore expects mongodb.Config for MongoDB connection:
+//
+//		mongodb.Config struct {
+//			Db   string `json:"db"`
+//			Host string `json:"host"`
+//			Port int    `json:"port"`
+//			DSN  string `json:"dsn"`
+//			User string `json:"password"`
+//			Pass string `json:"username"`
+//			TLS  bool   `json:"tls"`
+//	}
+func NewMetricStore(cfg *mongodb.Config) (*MetricStore, error) {
+
+	uri := options.Client().ApplyURI(cfg.DSN)
+	client, err := mongo.Connect(context.Background(), uri)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := &MetricStore{
+		collection:  client.Database(TelemetryDatabase).Collection(TelemetryCollection),
+		accumulator: make(map[string]MetricRow),
+		flushTicker: time.NewTicker(1 * time.Minute),
+		closeChan:   make(chan struct{}),
+	}
+
+	// Start the background flushing process
+	go ts.backgroundFlush()
+
+	return ts, nil
+}
+
+// Close flushes any remaining data and stops the background flush process
+func (ts *MetricStore) Close() {
+
+	ts.flushTicker.Stop()
+
+	if err := ts.flush(); err != nil {
+		slog.Error("Error flushing data on close", err)
+	}
+
+	close(ts.closeChan)
+}
+
+func (ts *MetricStore) Visited(job, url string) {
+	ts.cache(job, url, "visited", nil)
+}
+
+func (ts *MetricStore) Extracted(job, url string) {
+	ts.cache(job, url, "extracted", nil)
+}
+
+func (ts *MetricStore) Error(job, url string, errorMsg string) {
+	ts.cache(job, url, "error", &errorMsg)
+}
+
+func (ts *MetricStore) backgroundFlush() {
+	for {
+		select {
+		case <-ts.flushTicker.C:
+			if err := ts.flush(); err != nil {
+				fmt.Println("Error flushing data:", err)
+			}
+		case <-ts.closeChan:
+			// Perform one last flush before closing
+			if err := ts.flush(); err != nil {
+				fmt.Println("Error flushing data on close:", err)
+			}
+			return
+		}
+	}
+}
+
+func (ts *MetricStore) cache(job, url, eventType string, errorInfo *string) {
+	ts.mu.Lock()
+	defer ts.mu.Unlock()
+
+	key := fmt.Sprintf("%s_%s", job, url) // Unique key for job and URL
+	event := MetricRow{
+		Job:       job,
+		URL:       url,
+		EventType: eventType,
+		ErrorInfo: errorInfo,
+		Timestamp: time.Now(),
+	}
+	ts.accumulator[key] = event
+}
+
+func (ts *MetricStore) flush() error {
+	ts.mu.Lock()
+	defer ts.mu.Unlock()
+
+	var models []mongo.WriteModel
+	for _, event := range ts.accumulator {
+		filter := bson.M{"job": event.Job, "url": event.URL}
+		update := bson.M{
+			"$set": bson.M{
+				event.EventType: event.Timestamp,
+			},
+		}
+		if event.ErrorInfo != nil {
+			update["$set"].(bson.M)["error"] = *event.ErrorInfo
+		}
+		models = append(models, mongo.NewUpdateOneModel().SetFilter(filter).SetUpdate(update).SetUpsert(true))
+	}
+
+	if len(models) > 0 {
+		_, err := ts.collection.BulkWrite(context.Background(), models)
+		if err != nil {
+			return err
+		}
+		// Clear the accumulator after flushing
+		ts.accumulator = make(map[string]MetricRow)
+	}
+	return nil
+}
+
+func (ts *MetricStore) len() int {
+	ts.mu.Lock()
+	defer ts.mu.Unlock()
+	return len(ts.accumulator)
+}
---
+++ store/metric_test.go
@@ -0,0 +1,108 @@
+package store_test
+
+import (
+	"github.com/editorpost/donq/mongodb"
+	"github.com/editorpost/spider/store"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	testDbName = "test_test"
+)
+
+var (
+	mongoCfg *mongodb.Config
+)
+
+func TestMain(m *testing.M) {
+
+	res := map[string]any{
+		"db": "spider_meta",
+		"servers": []any{
+			map[string]any{
+				"host": "localhost",
+				"port": 27018,
+			},
+		},
+		"credential": map[string]any{
+			"username": "root",
+			"password": "nopass",
+		},
+	}
+
+	var err error
+	if mongoCfg, err = mongodb.ConfigFromResource(res); err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestTelemetryStore(t *testing.T) {
+
+	ts, err := store.NewMetricStore(mongoCfg)
+	if err != nil {
+		t.Fatalf("NewMetricStore() error = %v", err)
+	}
+
+	// Define test cases
+	tests := []struct {
+		name         string
+		events       []store.MetricRow
+		wantErr      bool
+		expectedBulk int
+	}{
+		{
+			name: "Single Event",
+			events: []store.MetricRow{
+				{Job: "job1", URL: "http://example.com", EventType: "visited", Timestamp: time.Now()},
+			},
+			wantErr:      false,
+			expectedBulk: 1,
+		},
+		{
+			name: "Multiple Events",
+			events: []store.MetricRow{
+				{Job: "job1", URL: "http://example.com", EventType: "visited", Timestamp: time.Now()},
+				{Job: "job2", URL: "http://example.net", EventType: "error", ErrorInfo: pointerToString("Page not found"), Timestamp: time.Now()},
+			},
+			wantErr:      false,
+			expectedBulk: 2,
+		},
+		{
+			name: "BulkWrite Error",
+			events: []store.MetricRow{
+				{Job: "job1", URL: "http://example.com", EventType: "visited", Timestamp: time.Now()},
+			},
+			wantErr:      true,
+			expectedBulk: 1,
+		},
+	}
+
+	// Run tests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			for _, event := range tt.events {
+				switch event.EventType {
+				case "visited":
+					ts.Visited(event.Job, event.URL)
+				case "extracted":
+					ts.Extracted(event.Job, event.URL)
+				case "error":
+					ts.Error(event.Job, event.URL, *event.ErrorInfo)
+				}
+			}
+		})
+	}
+
+	// Flush data
+	ts.Close()
+}
+
+func pointerToString(s string) *string {
+	return &s
+}
