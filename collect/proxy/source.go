a597b7eb873694a77ce73747b36f0068c8e34e05
estatie <estatie@proton.me>
2024-04-12 01:44:54 +0700

    Update sources urls.

--- collect/proxy/source.go
+++ collect/proxy/source.go
@@ -3,7 +3,7 @@ package proxy
 // LoadPublicList loads the valid list from public sources
 func LoadPublicLists() ([]string, error) {
 	return LoadStringLists([]string{
-		"https://api.proxyscrape.com/v1/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+		"https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&protocol=http&proxy_format=protocolipport&format=text&anonymity=Elite&timeout=20000",
 		"https://sunny9577.github.io/proxy-scraper/proxies.txt",
 		"https://www.proxy-list.download/api/v1/get?type=http",
 	})
