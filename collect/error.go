07b7dd039f8f91ec12041121fd61e2320931c54c
estatie <estatie@proton.me>
2024-04-12 05:06:05 +0700

    Retry on url errors.

--- collect/error.go
+++ collect/error.go
@@ -1,15 +1,23 @@
 package collect
 
 import (
+	"errors"
 	"github.com/gocolly/colly/v2"
 	"log/slog"
+	"net/url"
 )
 
 // error logging
 func (crawler *Crawler) error(resp *colly.Response, err error) {
 
-	if crawler.retry.Request(resp) {
-		return
+	// slog.Error(err.Error())
+
+	var urlErr *url.Error
+	if errors.As(err, &urlErr) {
+		err = urlErr.Err
+		if crawler.retry.Request(resp) {
+			return
+		}
 	}
 
 	// error if retry limit is reached
