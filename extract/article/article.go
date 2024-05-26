6ad72098b7c83ab4fa5e21f21e453af1be96db6f
estatie <estatie@proton.me>
2024-05-26 20:14:03 +0700

    Move constructor at the top of file.

--- extract/article/article.go
+++ extract/article/article.go
@@ -14,79 +14,6 @@ func init() {
 	validate = validator.New()
 }
 
-// Article represents a news article with various types of content.
-// This structure provides a flexible and universal foundation for storing and working with various types of content,
-// allowing for easy creation and modification of articles, as well as integration of media and social elements.
-type Article struct {
-	// ID is the unique identifier for the article, using a UUID4 format.
-	// This field is required to uniquely identify each article.
-	ID string `json:"article__id" validate:"required,uuid4"`
-
-	// Title is the title of the article, which is required and should be between 1 and 255 characters long.
-	// This field is crucial for SEO and display purposes.
-	Title string `json:"article__title" validate:"required,min=1,max=255"`
-
-	// Byline represents the author or authors of the article.
-	// This field is required and should be less than 255 characters long or empty.
-	Byline string `json:"article__byline" validate:"max=255"`
-
-	// Content contains the main content of the article in HTML format.
-	// This field is required and allows for rich text formatting and media integration.
-	Content string `json:"article__content" validate:"required"`
-
-	// TextContent contains the main text of the article without HTML tags.
-	// This field is required and is useful for text analysis and summaries.
-	TextContent string `json:"article__text_content" validate:"required"`
-
-	// Excerpt is a short summary or teaser of the article, which can be up to 500 characters long.
-	// This field is optional and useful for previews and snippets.
-	Excerpt string `json:"article__excerpt" validate:"omitempty,max=500"`
-
-	// Images is an array of images related to the article.
-	// Each image must follow its own validation rules.
-	Images []Image `json:"article__images" validate:"omitempty,dive"`
-
-	// Videos is an array of videos related to the article.
-	// Each video must follow its own validation rules.
-	Videos []Video `json:"article__videos" validate:"omitempty,dive"`
-
-	// Quotes is an array of quotes from social media related to the article.
-	// Each quote must follow its own validation rules.
-	Quotes []Quote `json:"article__quotes" validate:"omitempty,dive"`
-
-	// PublishDate is the date when the article was published.
-	// This field is required for chronological sorting and relevance.
-	PublishDate time.Time `json:"article__publish_date" validate:"required"`
-
-	// ModifiedDate is the date when the article was last modified.
-	// This field is required for tracking updates.
-	ModifiedDate time.Time `json:"article__modified_date"`
-
-	// Tags is an array of tags associated with the article.
-	// Each tag must be between 1 and 50 characters long.
-	Tags []string `json:"article__tags" validate:"omitempty,dive,min=1,max=50"`
-
-	// Source is the URL of the source of the article.
-	// This field is optional and should be a valid URL if provided.
-	Source string `json:"article__source" validate:"omitempty,url"`
-
-	// Language is the language of the article, using a code between 2 and 5 characters long.
-	// This field is required for localization and internationalization.
-	Language string `json:"article__language" validate:"required,min=2,max=5"`
-
-	// Category is the category of the article, which should be between 1 and 100 characters long.
-	// This field is required for organizing content.
-	Category string `json:"article__category" validate:"max=100"`
-
-	// SiteName is the name of the site from which the article was taken.
-	// This field is required and should be between 1 and 255 characters long.
-	SiteName string `json:"article__site_name" validate:"max=255"`
-
-	// AuthorSocialProfiles is an array of social profiles of the author.
-	// Each profile must follow its own validation rules.
-	AuthorSocialProfiles []SocialProfile `json:"article__author_social_profiles" validate:"omitempty,dive"`
-}
-
 // NewArticle creates a new Article with the provided data and returns a pointer to the Article.
 func NewArticle() *Article {
 	return &Article{
@@ -100,8 +27,32 @@ func NewArticle() *Article {
 	}
 }
 
+// Article represents a news article with various types of content.
+// This structure provides a flexible and universal foundation for storing and working with various types of content,
+// allowing for easy creation and modification of articles, as well as integration of media and social elements.
+type Article struct {
+	ID                   string          `json:"article__id" validate:"required,uuid4"`
+	Title                string          `json:"article__title" validate:"required,max=255"`
+	Byline               string          `json:"article__byline" validate:"max=255"`
+	Content              string          `json:"article__content" validate:"required"`
+	TextContent          string          `json:"article__text_content" validate:"required"`
+	Excerpt              string          `json:"article__excerpt" validate:"max=500"`
+	PublishDate          time.Time       `json:"article__publish_date" validate:"required"`
+	ModifiedDate         time.Time       `json:"article__modified_date"`
+	Images               []Image         `json:"article__images"`
+	Videos               []Video         `json:"article__videos"`
+	Quotes               []Quote         `json:"article__quotes"`
+	Tags                 []string        `json:"article__tags"`
+	Source               string          `json:"article__source" validate:"omitempty,url"`
+	Language             string          `json:"article__language" validate:"required,len=2"`
+	Category             string          `json:"article__category" validate:"max=255"`
+	SiteName             string          `json:"article__site_name" validate:"max=255"`
+	AuthorSocialProfiles []SocialProfile `json:"article__author_social_profiles"`
+}
+
 // Normalize validates the Article and its nested structures, logs any validation errors, and clears invalid fields.
 func (a *Article) Normalize() {
+	validate := validator.New()
 
 	err := validate.Struct(a)
 	if err != nil {
