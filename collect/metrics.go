f231e1af8f35ed934668f13173f8f4a9282a12b9
estatie <estatie@proton.me>
2024-06-02 09:25:37 +0700

    Add retry on metrics server connection.

--- collect/metrics.go
+++ collect/metrics.go
@@ -3,8 +3,10 @@ package collect
 import (
 	"fmt"
 	"github.com/VictoriaMetrics/metrics"
+	"github.com/avast/retry-go"
 	"github.com/gocolly/colly/v2"
 	"log/slog"
+	"net"
 	"strconv"
 	"time"
 )
@@ -28,11 +30,35 @@ type (
 	}
 )
 
-func NewMetrics(job, spider string) *Metrics {
+// NewMetrics creates a new Metrics instance
+func NewMetrics(job, spider, promUrl string) *Metrics {
+
+	// pusher
+	err := retry.Do(
+		func() error {
+			return metrics.InitPush(
+				promUrl, // "http://localhost:35021/api/v1/import/prometheus",
+				5*time.Second,
+				fmt.Sprintf(`job="%s", spider="%s"`, job, spider),
+				false,
+			)
+		},
+		retry.Attempts(5), retry.Delay(3*time.Second),
+	)
+
+	if err != nil {
+		slog.Error("failed to init metrics", slog.String("err", err.Error()))
+	}
+
 	return &Metrics{
 		jobID:    job,
 		spiderID: spider,
 	}
+}
+
+func (m *Metrics) Init() *Metrics {
+
+	return m
 }
 
 func (m *Metrics) OnRequest(req *colly.Request) {
@@ -91,8 +117,11 @@ func (m *Metrics) SetLatency(event string, req *colly.Request) {
 	// latency, seconds
 	latency := float64(time.Now().UnixMilli()-int64(start)) / 1000
 
+	// get host without port
+	host, _, _ := net.SplitHostPort(req.URL.Host)
+
 	// set metric name
-	labels := fmt.Sprintf(`job="%s", spider="%s"`, m.jobID, m.spiderID)
+	labels := fmt.Sprintf(`host="%s", path="%s"`, host, req.URL.Path)
 	metrics.GetOrCreateHistogram(fmt.Sprintf(`spider_%s_lat{%s}`, event, labels)).Update(latency)
 }
 
@@ -101,9 +130,20 @@ func (m *Metrics) Counter(event string) *metrics.Counter {
 	return metrics.GetOrCreateCounter(fmt.Sprintf(format, event, m.jobID, m.spiderID))
 }
 
+func (m *Metrics) CounterUrl(event, url string) *metrics.Counter {
+	format := `spider_%s_count{url="%s"}`
+	return metrics.GetOrCreateCounter(fmt.Sprintf(format, event, url))
+}
+
 func (m *Metrics) Gauge(event string) *metrics.Gauge {
 	format := `spider_%s_gauge{job="%s", spider="%s"}`
 	return metrics.GetOrCreateGauge(fmt.Sprintf(format, event, m.jobID, m.spiderID), nil)
+}
+
+func (m *Metrics) GaugeUrl(event, url string) *metrics.Gauge {
+	format := `spider_%s_gauge{url="%s"}`
+	return metrics.GetOrCreateGauge(fmt.Sprintf(format, event, url), nil)
+
 }
 
 func (m *Metrics) Now() time.Time {
