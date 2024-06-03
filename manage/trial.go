3f0f010ef97d86c2f307c990a0f089ae57e536cc
estatie <estatie@proton.me>
2024-06-03 16:10:10 +0700

    Add trial command to make limited dry run with given arguments.

---
+++ manage/trial.go
@@ -0,0 +1,29 @@
+package manage
+
+import (
+	"github.com/editorpost/spider/collect/config"
+	"github.com/editorpost/spider/extract"
+	"github.com/editorpost/spider/manage/setup"
+)
+
+// Trial spider against limited and return extracted data
+// It does not store the data, but uses proxy pool for requests.
+func Trial(args *config.Args, extractor extract.PipeFn) ([]*extract.Payload, error) {
+
+	args.SpiderID = "trial"
+	items := []*extract.Payload{}
+	deploy := &setup.Deploy{}
+
+	err := Start(args, deploy, extractor, func(payload *extract.Payload) error {
+
+		// the queue will stops automatically
+		// after args.ExtractLimit is reached
+		if len(items) < args.ExtractLimit {
+			items = append(items, payload)
+		}
+
+		return nil
+	})
+
+	return items, err
+}
---
+++ manage/trial_test.go
@@ -0,0 +1,40 @@
+//go:build e2e
+// +build e2e
+
+package manage_test
+
+import (
+	"fmt"
+	"github.com/editorpost/article"
+	"github.com/editorpost/spider/collect/config"
+	"github.com/editorpost/spider/extract"
+	"github.com/editorpost/spider/manage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestTrial(t *testing.T) {
+
+	args := &config.Args{
+		StartURL:        "https://thailand-news.ru/news/turizm/",
+		AllowedURL:      "https://thailand-news.ru/news/{turizm,puteshestviya}{any}",
+		ExtractURL:      "https://thailand-news.ru/news/{turizm,puteshestviya}/{some}",
+		ExtractLimit:    2,
+		Depth:           1,
+		ExtractSelector: ".node-article--full",
+		ProxyEnabled:    false,
+	}
+
+	articles, tErr := manage.Trial(args, extract.Article)
+	require.NoError(t, tErr)
+	assert.NotNil(t, articles)
+
+	for _, payload := range articles {
+		a, err := article.NewArticleFromMap(payload.Data)
+		assert.NoError(t, err)
+		fmt.Printf("%s: %s\n", a.Published.Format("2006-01-02"), a.Title)
+	}
+
+	fmt.Printf("Extracted %d items\n", len(articles))
+}
