97ea008a502398f5e5f75012a5c95bc8d92ab3a8
estatie <estatie@proton.me>
2024-06-12 10:38:00 +0700

    Extend tests on extract builder.

---
+++ extract/fields/builder_test.go
@@ -0,0 +1,299 @@
+package fields_test
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/editorpost/spider/extract/fields"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+)
+
+func TestExtract(t *testing.T) {
+
+	tc := []struct {
+		name     string
+		builders []fields.Builder
+		expected any
+		hasErr   bool
+		err      error
+	}{
+		{
+			"single",
+			[]fields.Builder{
+				&fields.Group{
+					Name:     "product",
+					Selector: ".product--full",
+					Limit:    1,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "title",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__title",
+						},
+						{
+							Name:         "price",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+					},
+				},
+			},
+			map[string]any{
+				"product": map[string]any{
+					"title": "Main Product Title",
+					"price": "99.99",
+				},
+			},
+			false,
+			nil,
+		},
+		{
+			"multiple",
+			[]fields.Builder{
+				&fields.Group{
+					Name:     "products",
+					Selector: ".product",
+					Limit:    2,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "title",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__title",
+						},
+						{
+							Name:         "price",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+					},
+				},
+			},
+			map[string]any{
+				"products": []any{
+					map[string]any{
+						"title": "Main Product Title",
+						"price": "99.99",
+					},
+					map[string]any{
+						"title": "Another Product Title",
+						"price": "49.99",
+					},
+				},
+			},
+			false,
+			nil,
+		},
+		{
+			"multiple, skip missing required",
+			[]fields.Builder{
+				&fields.Group{
+					Name:     "prices",
+					Selector: ".product__price",
+					Limit:    0,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "amount",
+							Cardinality:  1,
+							Required:     true,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+						{
+							Name:         "currency",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--currency",
+						},
+					},
+				},
+			},
+			map[string]any{
+				"prices": []any{
+					map[string]any{
+						"amount":   "99.99",
+						"currency": "USD",
+					},
+					map[string]any{
+						"amount":   "49.99",
+						"currency": "USD",
+					},
+					map[string]any{
+						"amount":   "0.99",
+						"currency": "USD",
+					},
+				},
+			},
+			false,
+			nil,
+		},
+		{
+			"multiple, skip missing required",
+			[]fields.Builder{
+				&fields.Group{
+					Name:     "buy",
+					Selector: ".product__price",
+					Limit:    1,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "amount",
+							Cardinality:  1,
+							Required:     true,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+						{
+							Name:         "currency",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--currency",
+						},
+					},
+				},
+				&fields.Group{
+					Name:     "sell",
+					Selector: ".product__price",
+					Limit:    2,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "amount",
+							Cardinality:  1,
+							Required:     true,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+						{
+							Name:         "currency",
+							Cardinality:  1,
+							InputFormat:  "html",
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--currency",
+						},
+					},
+				},
+				&fields.Field{
+					Name:         "title",
+					Cardinality:  1,
+					Required:     true,
+					InputFormat:  "html",
+					OutputFormat: []string{"text"},
+					Selector:     "head title",
+				},
+			},
+			map[string]any{
+				"buy": map[string]any{
+					"amount":   "99.99",
+					"currency": "USD",
+				},
+				"sell": []any{
+					map[string]any{
+						"amount":   "99.99",
+						"currency": "USD",
+					},
+					map[string]any{
+						"amount":   "49.99",
+						"currency": "USD",
+					},
+				},
+				"title": "Product Page Example",
+			},
+			false,
+			nil,
+		},
+		{
+			"nil result",
+			[]fields.Builder{
+				&fields.Group{
+					Name:     "prices",
+					Selector: ".product__price--amount",
+					Limit:    1,
+					Required: true,
+					Fields: []*fields.Field{
+						{
+							Name:         "amount",
+							Cardinality:  1,
+							InputFormat:  "html",
+							Required:     true,
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--amount",
+						},
+						{
+							Name:         "currency",
+							Cardinality:  1,
+							InputFormat:  "html",
+							Required:     true,
+							OutputFormat: []string{"text"},
+							Selector:     ".product__price--currency",
+						},
+					},
+				},
+			},
+			nil,
+			true,
+			fields.ErrRequiredFieldMissing,
+		},
+	}
+
+	// use testify assert
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+
+			// check error
+			skipExpectedErr := func(actual error) bool {
+
+				if actual == nil {
+					// continue test case execution
+					return false
+				}
+
+				// force error if not expected
+				if !c.hasErr {
+					assert.NoError(t, actual)
+				}
+
+				// check error instance
+				if c.err != nil {
+					assert.ErrorIs(t, c.err, actual)
+				}
+
+				// stops test case execution
+				return true
+			}
+
+			fn, err := fields.Extract("", c.builders...)
+			if skip := skipExpectedErr(err); skip {
+				return
+			}
+
+			// compare values
+			read := strings.NewReader(GetTestFieldsHTML(t))
+			dom, err := goquery.NewDocumentFromReader(read)
+			require.NoError(t, err)
+
+			values, err := fn(dom.Selection)
+			if skip := skipExpectedErr(err); skip {
+				return
+			}
+
+			assert.Equal(t, c.expected, values)
+		})
+	}
+}
