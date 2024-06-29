6f975dcf421a2fd001c96493e9dc86c59cc542c5
estatie <estatie@proton.me>
2024-06-30 03:15:56 +0700

    Update executable flags test.

--- main_test.go
+++ main_test.go
@@ -3,70 +3,35 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"github.com/editorpost/spider/collect/config"
-	"github.com/editorpost/spider/extract/fields"
+	"github.com/editorpost/spider/manage/setup"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestFlags(t *testing.T) {
 
-	argsJson, expectedArgs := NewArgs()
-	treeJson, expectedTree := NewFields()
+	jsonStr := `{
+		"ID": "test",
+		"StartURL": "http://example.com",
+		"ExtractLimit": 10,
+		"ExtractEntities": ["person", "organization"],
+		"ExtractFields": [
+			{
+				"Name": "name",
+				"Selector": "h1"
+			}
+		]
+	}`
 
-	flag.Set("args", argsJson)
-	flag.Set("fields", treeJson)
-	flag.Set("entities", "html,article")
+	s := &setup.Spider{}
+	require.NoError(t, json.Unmarshal([]byte(jsonStr), s))
+
+	flag.Set("spider", jsonStr)
 	flag.Set("cmd", "start")
 
 	cmd, spider := Flags()
 
 	assert.Equal(t, "start", cmd)
-	assert.Equal(t, expectedArgs, spider.Args)
-	assert.Equal(t, expectedTree, spider.ExtractFields)
-}
-
-func NewArgs() (js string, args *config.Args) {
-	args = &config.Args{
-		ID:              "ready-check",
-		Name:            "Ready Check",
-		StartURL:        "https://example.com",
-		AllowedURL:      "https://example.com/{any}",
-		ExtractURL:      "https://example.com/articles/{any}",
-		ExtractSelector: "article",
-		ExtractLimit:    1,
-		UseBrowser:      true,
-		Depth:           1,
-		UserAgent:       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-		ProxyEnabled:    true,
-		ProxySources:    []string{},
-	}
-
-	// marshal args to JSON
-	b, _ := json.Marshal(args)
-	return string(b), args
-}
-
-func NewFields() (js string, f []*fields.Field) {
-
-	f = []*fields.Field{
-		{
-			Name:         "title",
-			Cardinality:  1,
-			InputFormat:  "html",
-			OutputFormat: []string{"text"},
-			Selector:     ".product__title",
-		},
-		{
-			Name:         "price",
-			Cardinality:  1,
-			InputFormat:  "html",
-			OutputFormat: []string{"text"},
-			Selector:     ".product__price--amount",
-		},
-	}
-
-	// marshal fields to JSON
-	b, _ := json.Marshal(f)
-	return string(b), f
+	assert.Equal(t, s, spider)
 }
