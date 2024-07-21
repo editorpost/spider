a4589fb81ccb1c54068427140c3499551e3dac48
estatie <estatie@proton.me>
2024-07-22 04:34:16 +0700

    Clean test bucket func.

--- tester/deploy.go
+++ tester/deploy.go
@@ -3,8 +3,12 @@ package tester
 import (
 	"github.com/editorpost/spider/manage/setup"
 	"github.com/editorpost/spider/store"
+	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
+
+const DeployBucketFolder = "./testdata"
 
 func TestDeploy(t *testing.T) *setup.Deploy {
 
@@ -13,10 +17,15 @@ func TestDeploy(t *testing.T) *setup.Deploy {
 	return &setup.Deploy{
 		Bucket: store.Bucket{
 			Name:      "local",
-			Endpoint:  "./testdata",
+			Endpoint:  DeployBucketFolder,
 			PublicURL: "http://localhost:9000",
 		},
 		VictoriaMetricsUrl: "http://metrics.spider.svc:8429/api/v1/import/prometheus",
 		VictoriaLogsUrl:    "http://logs.spider.svc:9428",
 	}
+}
+
+func CleanTestBucket(t *testing.T) {
+	t.Helper()
+	require.NoError(t, os.RemoveAll(DeployBucketFolder))
 }
