115d44fc876183789a8a945ce1fb64927a0332be
estatie <estatie@proton.me>
2024-07-22 06:41:12 +0700

    Lock claims operations.

--- extract/media/claims.go
+++ extract/media/claims.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/url"
 	"strings"
+	"sync"
 )
 
 // Claim claim to get src media and save it to dst.
@@ -23,7 +24,8 @@ type Claims struct {
 	// publicURL is a prefix of the replaced media url.
 	publicURL string
 	// claims keyed with source url
-	claims map[string]Claim
+	claims map[string]*Claim
+	mute   *sync.Mutex
 }
 
 // NewClaims creates a new Claim for each image and replace src path in document and selection.
@@ -32,7 +34,8 @@ func NewClaims(publicURL string) *Claims {
 
 	claims := &Claims{
 		publicURL: publicURL,
-		claims:    make(map[string]Claim),
+		claims:    make(map[string]*Claim),
+		mute:      &sync.Mutex{},
 	}
 
 	return claims
@@ -87,20 +90,48 @@ func (list *Claims) ExtractAndReplace(selection *goquery.Selection) *Claims {
 }
 
 func (list *Claims) Add(c Claim) *Claims {
-	list.claims[c.Src] = c
+
+	list.mute.Lock()
+	defer list.mute.Unlock()
+
+	list.claims[c.Src] = &c
 	return list
 }
 
 // Request Claim for uploading by Dst url.
-func (list *Claims) Request(byDestinationURL string) *Claims {
+func (list *Claims) Request(uri string) *Claims {
 
-	for _, claim := range list.claims {
-		if claim.Dst == byDestinationURL {
-			claim.Requested = true
-		}
+	list.mute.Lock()
+	defer list.mute.Unlock()
+
+	if claim := list.byDestination(uri); claim != nil {
+		claim.Requested = true
+		return list
+	}
+
+	if claim := list.bySource(uri); claim != nil {
+		claim.Requested = true
 	}
 
 	return list
+}
+
+func (list *Claims) byDestination(u string) *Claim {
+	for _, claim := range list.claims {
+		if claim.Dst == u {
+			return claim
+		}
+	}
+	return nil
+}
+
+func (list *Claims) bySource(u string) *Claim {
+	for _, claim := range list.claims {
+		if claim.Src == u {
+			return claim
+		}
+	}
+	return nil
 }
 
 // Done Claim marks Claim as uploaded.
@@ -108,7 +139,10 @@ func (list *Claims) Done(byDestinationURL string) *Claims {
 
 	for _, claim := range list.claims {
 		if claim.Dst == byDestinationURL {
+			list.mute.Lock()
 			claim.Done = true
+			list.mute.Unlock()
+			return list
 		}
 	}
 
@@ -116,9 +150,9 @@ func (list *Claims) Done(byDestinationURL string) *Claims {
 }
 
 // Requested returns a list of requested claims.
-func (list *Claims) Requested() []Claim {
+func (list *Claims) Requested() []*Claim {
 
-	requested := make([]Claim, 0, len(list.claims))
+	requested := make([]*Claim, 0, len(list.claims))
 	for _, claim := range list.claims {
 		if claim.Requested {
 			requested = append(requested, claim)
@@ -128,9 +162,9 @@ func (list *Claims) Requested() []Claim {
 }
 
 // Uploaded returns a list of uploaded claims.
-func (list *Claims) Uploaded() []Claim {
+func (list *Claims) Uploaded() []*Claim {
 
-	uploaded := make([]Claim, 0, len(list.claims))
+	uploaded := make([]*Claim, 0, len(list.claims))
 	for _, claim := range list.claims {
 		if claim.Done {
 			uploaded = append(uploaded, claim)
@@ -139,9 +173,9 @@ func (list *Claims) Uploaded() []Claim {
 	return uploaded
 }
 
-func (list *Claims) All() []Claim {
+func (list *Claims) All() []*Claim {
 
-	all := make([]Claim, 0, len(list.claims))
+	all := make([]*Claim, 0, len(list.claims))
 	for _, claim := range list.claims {
 		all = append(all, claim)
 	}
