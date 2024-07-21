f7a3ceee020445f53e127dfd4dc75b8e9d4f8c3e
estatie <estatie@proton.me>
2024-07-22 00:40:38 +0700

    Add in-memory storage for colly visited URLs. No bloom filter. URLs loaded from S3 on crawler start and uploaded back on finish.

--- store/collect.go
+++ store/collect.go
@@ -1,37 +1,102 @@
 package store
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 	"github.com/gocolly/colly/v2/storage"
-)
-
-const (
-	CollectorVisited  = "visited"
-	CollectorCookies  = "cookies"
-	ExtractorResults  = "crawled"
-	StoreRequestIDKey = "requestID"
-	StoreVisitedKey   = "visited"
-	StoreHostKey      = "host"
-	StoreCookiesKey   = "cookies"
+	"net/http/cookiejar"
+	"net/url"
+	"sync"
 )
 
 type CollectStore interface {
 	storage.Storage
-	Drop() error
+	Reset() error
 }
 
-func NewCollectStore(dbName, dsn string) (CollectStore, error) {
+// CollectStorage in-memory colly storage backed by S3
+// @see CollectHistory.Init and CollectStorage.Shutdown
+type CollectStorage struct {
+	b     Bucket
+	store Storage
+	// Based on colly storage.InMemoryStorage
+	// @source github.com/gocolly/colly/v2@v2.1.1-0.20240327170223-5224b972e22b/storage/storage.go
+	visitedURLs map[uint64]bool
+	lock        *sync.RWMutex
+	jar         *cookiejar.Jar
+}
 
-	if dsn == "" {
-		return nil, errors.New("collector store dsn is empty")
+func NewCollectStorage(spiderID string, b Bucket) (*CollectStorage, error) {
+
+	jar, _ := cookiejar.New(nil)
+	folder := fmt.Sprintf(CollectFolder, spiderID)
+
+	store, err := NewStorage(b, folder)
+	if err != nil {
+		return nil, fmt.Errorf("extract store s3 client: %w", err)
 	}
 
-	// switch store by dsn prefix database, e.g. mongodb://, postgres://
-	switch {
-	case dsn[:8] == "mongodb://":
-		return NewMongoCollectStore(dbName, dsn)
+	return &CollectStorage{
+		b:           b,
+		store:       store,
+		visitedURLs: make(map[uint64]bool),
+		lock:        &sync.RWMutex{},
+		jar:         jar,
+	}, nil
+}
+
+func (s *CollectStorage) Shutdown() error {
+
+	b, err := json.Marshal(s.visitedURLs)
+	if err != nil {
+		return err
 	}
 
-	// return error if dsn not supported
-	return nil, errors.New("can't create store for dsn: " + dsn + ", only mongodb:// and postgres:// are supported")
+	return s.store.Save(b, VisitedFile)
+}
+
+// Init initializes CollectStorage
+func (s *CollectStorage) Init() error {
+
+	b, err := s.store.Load(VisitedFile)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, &s.visitedURLs)
+}
+
+func (s *CollectStorage) Reset() error {
+	return s.store.Delete(VisitedFile)
+}
+
+// Visited implements Storage.Visited()
+func (s *CollectStorage) Visited(requestID uint64) error {
+	s.lock.Lock()
+	s.visitedURLs[requestID] = true
+	s.lock.Unlock()
+	return nil
+}
+
+// IsVisited implements Storage.IsVisited()
+func (s *CollectStorage) IsVisited(requestID uint64) (bool, error) {
+	s.lock.RLock()
+	visited := s.visitedURLs[requestID]
+	s.lock.RUnlock()
+	return visited, nil
+}
+
+// Cookies implements Storage.Cookies()
+func (s *CollectStorage) Cookies(u *url.URL) string {
+	return storage.StringifyCookies(s.jar.Cookies(u))
+}
+
+// SetCookies implements Storage.SetCookies()
+func (s *CollectStorage) SetCookies(u *url.URL, cookies string) {
+	s.jar.SetCookies(u, storage.UnstringifyCookies(cookies))
+}
+
+// Close implements Storage.Close()
+func (s *CollectStorage) Close() error {
+	return nil
 }
